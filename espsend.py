a8d772738d9f4ec10cc4e6d328456ace6cc58b02
bhushananokar <75465521+bhushananokar@users.noreply.github.com>
2024-08-04 13:06:43 +0530

    Update espsend.py

--- espsend.py
+++ espsend.py
@@ -6,22 +6,22 @@ from firebase_admin import credentials, storage
 import os
 
 # Firebase initialization
-cred = credentials.Certificate(r'parking-project-309c3-firebase-adminsdk-ix0ws-1bb1099dd7.json')
+cred = credentials.Certificate() #path to your firbase admin sdk
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'parking-project-309c3.appspot.com'
+    'storageBucket': 'Your-project-id.appspot.com'
 })
 bucket = storage.bucket()
 
 def upload_to_firebase(frame, frame_counter):
-    # Save the frame locally
+
     local_file = f'frame_{frame_counter}.jpg'
     cv2.imwrite(local_file, frame)
     
-    # Upload the frame to Firebase Storage
+ 
     blob = bucket.blob(f'webcam_frames/frame_{frame_counter}.jpg')
     blob.upload_from_filename(local_file)
     
-    # Remove the local file
+ 
     os.remove(local_file)
     print(f'Uploaded frame_{frame_counter}.jpg to Firebase Storage')
 
@@ -30,19 +30,16 @@ def main():
     frame_counter = 0
 
     while True:
-        # Capture frame from ESP32-CAM
+    
         img_resp = urllib.request.urlopen(url)
         imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
         im = cv2.imdecode(imgnp, -1)
 
-        # Upload frame to Firebase
         upload_to_firebase(im, frame_counter)
         frame_counter += 1
 
-        # Display the frame
         cv2.imshow('Image', im)
 
-        # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
